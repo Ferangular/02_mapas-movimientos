0e68122bfadfbbc2764f08b116aa398bdc4446f7
firthuns <firthunsnpm@gmail.com>
2026-04-24 21:57:55 +0200

    feat: remove tokens

--- angular-socket-map/src/environments/environment.ts
+++ angular-socket-map/src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   websocketUrl: 'ws://localhost:3200',
   mapbox: {
-    accessToken: 'pk.eyJ1IjoiZmlydGh1bnMyMDI2IiwiYSI6ImNtb2Npa2dwcjAxMDcycXIxZDZ2c25pdncifQ.kKJEf9kvS-_mM-Fk2dYF-Q',
+    accessToken: '',
   },
 };
